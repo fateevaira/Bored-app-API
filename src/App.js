c1d4ffb4269dfcca88d44203e59ed5e014925fe0
fateeva ira <fateevaira@ukr.net>
2022-03-29 13:41:08 +0300

    add app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import video from './Octagon.mp4';
 import './App.css';
 
 function App() {
+
+  const [showAdvice, setShowAdvice] = useState('');
+
+  useEffect(() => {
+    getAdvice()
+  }, [])
+
+  const getAdvice = async() => {
+    const response = await fetch(`http://www.boredapi.com/api/activity/`);
+    const data = await response.json();
+    setShowAdvice(data.activity);
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <video autoPlay muted loop>
+          <source src={video} type='video/mp4'/>
+        </video>
+
+        <h1>What to do if you're bored</h1>
+      </div>
+
+      <div className='container'>
+        <h2>{showAdvice}</h2>
+      </div>
+     
+     <div className='container'>
+       <button className='btn' onClick={getAdvice}>Show next advice</button>
+     </div>
     </div>
   );
 }
